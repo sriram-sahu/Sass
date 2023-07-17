b4236aac529cbb55ef59550b6af26c6b7ed7c28f
sriram-sahu <sriramsahu510@gamail.com>
2023-07-17 19:33:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./sass/index.css";
+
+localStorage.setItem("name", "sriram");
+const App = () => {
+  return <div className='app'>App</div>;
+};
+
+export default App;
